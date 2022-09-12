f5be42b806b1fdb5b0250e1c1ac95ac908c4dbff
MbonuCharles <saintabuchi@gmail.com>
2022-09-12 14:10:34 -0700

    GithubUsers.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import GithubUsers from './components/GithubUsers';
+
+function App() {
+  return (
+    <div className="App">
+      <GithubUsers/>
+    </div>
+  );
+}
+
+export default App;
